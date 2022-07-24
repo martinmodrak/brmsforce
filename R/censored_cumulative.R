dc07398f346b7202b26fdc432c481aff5efc541a
martinmodrak <modrak.mar@gmail.com>
2022-07-24 22:17:46 +0200

    Censored cumulative vignette WIP

--- R/censored_cumulative.R
+++ R/censored_cumulative.R
@@ -2,7 +2,7 @@
 #'
 #' Use as:
 #' brm(min | vint(max)  ~ predictors , family = censored_cumulative_logit(), stanvars = censored_cumulative_logit_stanvars(), ...)
-#' Non-censored observations mean min == max.
+#' Non-censored observations mean min == max. See `vignette("censored_cumulative_usage")` for more details.
 #' @export
 #' @importFrom brms custom_family
 censored_cumulative_logit <- function() {
@@ -22,8 +22,14 @@ censored_cumulative_logit_stanvars <- function() {
       if(min > max) {
         reject("min > max");
       }
+      if(max > size(Intercept) + 1) {
+        reject("max > number of categories");
+      }
       if(min == max) {
         return(ordered_logistic_lpmf(min | mu, Intercept));
+      } else if(min == 1 && max == size(Intercept) + 1) {
+        // The range covers all possibilities
+        return 0;
       } else {
         vector[max - min + 1] vals;
         for(i in min:max) {
